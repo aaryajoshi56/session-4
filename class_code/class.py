60a3b5308dc835bb8dda77e718a8f4e51b9e65fb
C. Todd Lombardo <1851211+iamctodd@users.noreply.github.com>
2025-02-03 18:11:34 +0000

    sol0n

---
+++ class_code/class.py
@@ -0,0 +1,14 @@
+students = [{"name" : "Hermione",
+  "house" : "Gryffindor",
+  "patronus" : "Otter"},
+ {"name" : "Harry",
+  "house" : "Gryffindor",
+  "patronus" : "Stag"},
+ {"name" : "Ron",
+  "house" : "Gryffindor",
+  "patronus" : "Jack Russell Terrier"}, 
+ {"name" : "Draco",
+  "house" : "Slytherin",
+  "patronus" : None},]
+
+print(students[2]["patronus"])
